9c1be70e7dc153f90134cadb7df58a801ddd5356
J2ON <63008127+J2on@users.noreply.github.com>
2024-01-23 22:10:21 +0900

    [Silver III] Title: queuestack, Time: 44 ms, Memory: 3216 KB -BaekjoonHub

--- 백준/Silver/24511. queuestack/queuestack.cc
+++ 백준/Silver/24511. queuestack/queuestack.cc
@@ -10,13 +10,15 @@ int main() {
   
   int n;
   cin >> n;
-  
+
+  // data structure의 종류 확인
   int dsList[n];
   stack<int> st;
   for(int i=0; i<n; i++){
     cin >> dsList[i];  
   }
-
+  // Queue인 경우에만 stack에 쌓는다. 
+  // Stack인 경우엔 바뀌지 않으니 
   int temp;
   for(int i:dsList){
     cin >> temp;
@@ -24,7 +26,7 @@ int main() {
       st.push(temp);
     }
   }
-
+  // 뒤에 입력될 수를 queue에 담는다.
   queue<int> q;
   int m;
   cin >> m;
@@ -32,13 +34,13 @@ int main() {
     cin >> temp;
     q.push(temp);
   }
-
+  // stack에 있는 수 먼저 출력
   while(!st.empty() && m != 0){
     cout << st.top() << ' ';
     st.pop();
     m--;
   }
-
+  // 모자르다면 q에 있는 수도 출력
   if(m != 0){
     for(int i=0; i<m; i++){
       cout << q.front() << ' ';
