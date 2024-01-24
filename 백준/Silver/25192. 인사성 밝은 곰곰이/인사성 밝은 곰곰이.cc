a5cb604ff317929665f91da7a8349cbda4a6e25f
J2ON <63008127+J2on@users.noreply.github.com>
2024-01-24 16:13:58 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 124 ms, Memory: 8760 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <set>
+using namespace std;
+
+int main(){
+  int num;
+  cin >> num;
+  set<string> nameSet;
+  
+  string str;
+  int count = 0;
+  for(int i =0; i< num; i++){
+    cin >> str;
+
+    if(str == "ENTER"){
+      nameSet.clear();
+    }
+    else{
+      if(nameSet.find(str) == nameSet.end()){
+        nameSet.insert(str);
+        count++;
+      }
+    }
+  }
+  cout << count;
+}
