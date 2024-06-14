ac222953ed75d9c59b8fae4d55acae529511c6e5
J2ON <63008127+J2on@users.noreply.github.com>
2024-06-15 04:42:38 +0900

    [Bronze II] Title: 우유 축제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/14720. 우유 축제/우유 축제.cc
+++ 백준/Bronze/14720. 우유 축제/우유 축제.cc
@@ -2,14 +2,7 @@
 #include <vector>
 using namespace std;
 
-
-// 0 시작, 0 -> 1 -> 2 ->
-
-
 int main() {
-
-	
-
 	int n;
 	cin >> n;
 	vector<int> markets;
@@ -23,33 +16,13 @@ int main() {
 	
 	int max = 0;
 	int cnt = 0;
-	int milk[3] = { 0, 1, 2 };
 	for (auto& a : markets)
 	{
 		if (cnt % 3 == a)
 		{
 			cnt++;
 		}
-		else
-		{
-			/*if (cnt > max)
-			{
-				max = cnt;
-			}
-
-			cnt = 0;
-			if (a == 0) {
-				cnt = 1;
-			}*/
-		}
-
-		
 	}
 
-	if (cnt > max)
-	{
-		max = cnt;
-	}
-
-	cout << max;
+	cout << cnt;
 }
